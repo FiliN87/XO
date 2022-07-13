d42722a14314c120e30b484a6e345b2ecb778d80
FiliN <ermakov8709@rambler.ru>
2022-07-13 21:01:26 +0300

    add users

--- src/main/java/org/filin/UsersQueue.java
+++ src/main/java/org/filin/UsersQueue.java
@@ -65,27 +65,27 @@ public class UsersQueue {
             return result;
         }
     }
-    public class Model {
-        private static Model instance = new Model();
-
-        private List<User> model;
-
-        public static Model getInstance() {
-            return instance;
-        }
-
-        private Model() {
-            model = new ArrayList<>();
-        }
-
-        public void add(User user) {
-            model.add(user);
-        }
-
-        public List<String> list() {
-            return model.stream()
-                    .map(User::getName)
-                    .collect(Collectors.toList());
-        }
-    }
+//    public class Model {
+//        private static Model instance = new Model();
+//
+//        private List<User> model;
+//
+//        public static Model getInstance() {
+//            return instance;
+//        }
+//
+//        private Model() {
+//            model = new ArrayList<>();
+//        }
+//
+//        public void add(User user) {
+//            model.add(user);
+//        }
+//
+//        public List<String> list() {
+//            return model.stream()
+//                    .map(User::getName)
+//                    .collect(Collectors.toList());
+//        }
+//    }
 }
