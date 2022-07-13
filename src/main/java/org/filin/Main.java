11e2bf3d3356f67e210bcbe681a5ecc4e8050f9c
FiliN <ermakov8709@rambler.ru>
2022-07-13 20:30:03 +0300

    add users

--- src/main/java/org/filin/Main.java
+++ src/main/java/org/filin/Main.java
@@ -1,20 +1,32 @@
 package org.filin;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.Documented;
 
 @SpringBootApplication
 @Log4j2
 public class Main {
+
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+//        SpringApplication.run(Main.class, args);
         log.trace("level-1");
         log.debug("level-2");
         log.info("level-3");
         log.warn("level-4");
         log.error("level-5");
         log.fatal("level-6");
+        new Main();
+
+    }
+
+    public Main() {
+        UsersQueue usersQueue = new UsersQueue();
+        usersQueue.addList();
     }
 }
