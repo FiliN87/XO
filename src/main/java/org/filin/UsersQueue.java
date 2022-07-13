fffdc4cca6b78d4893a38d4a436ed7e0bbad1993
FiliN <ermakov8709@rambler.ru>
2022-07-13 20:56:05 +0300

    add users

--- src/main/java/org/filin/UsersQueue.java
+++ src/main/java/org/filin/UsersQueue.java
@@ -27,28 +27,22 @@ public class UsersQueue {
 
         public User() {
         }
-
         public User(String name, String password) {
             this.name = name;
             this.password = password;
         }
-
         public String getName() {
             return name;
         }
-
         public void setName(String name) {
             this.name = name;
         }
-
         public String getPassword() {
             return password;
         }
-
         public void setPassword(String password) {
             this.password = password;
         }
-
         @Override
         public String toString() {
             return "User{" +
@@ -56,25 +50,42 @@ public class UsersQueue {
                     ", password='" + password + '\'' +
                     '}';
         }
-
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
-
             User user = (User) o;
-
             if (name != null ? !name.equals(user.name) : user.name != null) return false;
             return password != null ? password.equals(user.password) : user.password == null;
-
         }
-
         @Override
         public int hashCode() {
             int result = name != null ? name.hashCode() : 0;
             result = 31 * result + (password != null ? password.hashCode() : 0);
             return result;
         }
+    }
+    public class Model {
+        private static Model instance = new Model();
 
+        private List<User> model;
+
+        public static Model getInstance() {
+            return instance;
+        }
+
+        private Model() {
+            model = new ArrayList<>();
+        }
+
+        public void add(User user) {
+            model.add(user);
+        }
+
+        public List<String> list() {
+            return model.stream()
+                    .map(User::getName)
+                    .collect(Collectors.toList());
+        }
     }
 }
